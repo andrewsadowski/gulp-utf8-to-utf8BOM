2783d971e44409587725073682ae850894f351c7
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-04-15 18:26:34 -0700

    added file-ext gulp-plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 const gulp = require('gulp');
 const bom = require('gulp-bom');
+var ext_replace = require('gulp-ext-replace');
 
-gulp.task('default', () => {
+gulp.task('bom-ify', () => {
   gulp
     .src('./test-files/*.srt')
     .pipe(bom())
     .pipe(gulp.dest('dist'));
 });
+
+gulp.task('change-ext', () => {
+  gulp
+    .src('./ext-files/*.txt')
+    .pipe(ext_replace('.srt'))
+    .pipe(gulp.dest('dist-ext'));
+});
+
+gulp.task('default', ['bom-ify', 'change-ext']);
